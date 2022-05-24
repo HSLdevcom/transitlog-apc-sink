55de50e58f15fb3a88bb4ce8dfaa8325ae06bb7b
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-05-24 10:00:27 +0300

    Log if service is unhealthy

--- src/main/kotlin/fi/hsl/transitlog/MessageHandler.kt
+++ src/main/kotlin/fi/hsl/transitlog/MessageHandler.kt
@@ -22,8 +22,16 @@ class MessageHandler(private val pulsarApplicationContext: PulsarApplicationCont
 
     private var lastAcknowledgedMessageTime = System.nanoTime()
 
-    val isHealthy: Boolean
-        get() = Duration.ofNanos(System.nanoTime() - lastAcknowledgedMessageTime)  < pulsarApplicationContext.config!!.getDuration("application.unhealthyIfNoAck")
+    fun isHealthy(): Boolean {
+        val timeSinceLastAck = Duration.ofNanos(System.nanoTime() - lastAcknowledgedMessageTime)
+        val healthy = timeSinceLastAck < pulsarApplicationContext.config!!.getDuration("application.unhealthyIfNoAck")
+
+        if (!healthy) {
+            log.warn { "Service unhealthy, last message was acknowledged ${timeSinceLastAck.seconds} seconds ago" }
+        }
+
+        return healthy
+    }
 
     private fun createDbConnection(): Connection {
         val dbAddress = pulsarApplicationContext.config!!.getString("db.address")
